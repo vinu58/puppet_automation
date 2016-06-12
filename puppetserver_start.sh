bf17a853ad8644717a2140cf31d75f1999b281c0
Linux Freak <vinu58@users.noreply.github.com>
2016-06-13 03:27:37 +0530

    A parameter has been added

--- puppetserver_start.sh
+++ puppetserver_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Script to start docker
 
+v1=24
+v2=52
+
 #session deals with starting a container with image
 masterimg=`docker images | grep puppetmaster | awk -F " " '{print $3}'`
 clientimg=`docker images | grep -w puppetcli | awk -F " " '{print $3}'`
@@ -9,10 +12,10 @@ docker run -it -d $masterimg /bin/bash
 docker run -it -d $clientimg /bin/bash
  
 #session deals with configuring a container
-puppetmasps=`docker ps | grep 24 | awk -F " " '{print $1}'`
+puppetmasps=`docker ps | grep $v1 | awk -F " " '{print $1}'`
 `cat /root/puppet/puppetmaster.sh | sudo docker exec -i $puppetmasps sh -c 'cat > /root/puppetmaster.sh'`
 
-puppetclips=`docker ps | grep 52 | awk -F " " '{print $1}'`
+puppetclips=`docker ps | grep $v2 | awk -F " " '{print $1}'`
 `cat /root/puppet/puppetclient.sh | sudo docker exec -i $puppetclips sh -c 'cat > /root/puppetclient.sh'`
 
 value=`docker inspect $puppetmasps | grep -i ipaddress | awk -F " " '{print $2}' | sed 's/,//' |  tr -d '"'`
